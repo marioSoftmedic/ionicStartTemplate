1a05f63a5288ed41bb669d13892d09753b3d8ead
Mario Inostroza <mario.inostroza.m@gmail.com>
2022-07-03 21:18:38 -0300

    Autenticación en IOS ajustes en XCODE

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,6 +8,16 @@ import {
 } from "@ionic/react";
 import "./styles.css";
 import { GoogleAuth } from "@codetrix-studio/capacitor-google-auth";
+import { isPlatform } from "@ionic/react";
+import {
+  SignInWithApple,
+  SignInWithAppleResponse,
+  SignInWithAppleOptions,
+} from "@capacitor-community/apple-sign-in";
+
+const ANDROID = isPlatform("android");
+const IOS = isPlatform("ios");
+const DESKTOP = isPlatform("desktop");
 
 const Home: React.FC = () => {
   GoogleAuth.initialize();
@@ -16,6 +26,25 @@ const Home: React.FC = () => {
     const user = await GoogleAuth.signIn();
 
     console.log(user);
+  }
+  async function signInWithApple() {
+    let options: SignInWithAppleOptions = {
+      clientId: "com.your.webservice",
+      redirectURI: "https://www.yourfrontend.com/login",
+      scopes: "email name",
+      state: "12345",
+      nonce: "nonce",
+    };
+    SignInWithApple.authorize(options)
+      .then((result: SignInWithAppleResponse) => {
+        // Handle user information
+        // Validate token with server and create new session
+        console.log("hola");
+      })
+      .catch((error) => {
+        // Handle error
+        console.log("errores");
+      });
   }
   return (
     <IonPage>
@@ -30,7 +59,12 @@ const Home: React.FC = () => {
             <IonTitle size="large">Home 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonButton onClick={signIn}>Logging with Google2</IonButton>
+        {IOS && (
+          <IonButton onClick={signInWithApple}>Login with APPLE</IonButton>
+        )}
+
+        {ANDROID && <IonButton onClick={signIn}>Login with Google</IonButton>}
+        {DESKTOP && <IonButton onClick={signIn}>Login with Google</IonButton>}
       </IonContent>
     </IonPage>
   );
